e6932517268bb341a110252e8c79d13b4c0a55d4
danleyzberg <dan.leyzberg@gmail.com>
2014-12-19 20:27:43 -0500

    mobile friendly

--- cos126/javascript/smaller-header-on-scroll.js
+++ cos126/javascript/smaller-header-on-scroll.js
@@ -1,30 +1,22 @@
 // source: http://callmenick.com/2014/02/18/create-an-animated-resizing-header-on-scroll/
 
-function init() {
 
-
-	window.addEventListener('scroll', function(e){
-			var nav = document.getElementById("nav");
-			var distanceY = window.pageYOffset || document.documentElement.scrollTop;
-			var shrinkOn = 300;
-			if (distanceY > shrinkOn) {
-					classie.add(nav,"smaller");
-			} else {
-					if (classie.has(nav,"smaller")) {
-							classie.remove(nav,"smaller");
-					}
-			}
-	});
-
-	document.getElementById("nav").addEventListener("mouseover", function(e) {
+window.onload = function() {
+	window.addEventListener('scroll',function() {
 		var nav = document.getElementById("nav");
-		if (classie.has(document.getElementById("nav"),"smaller")) {
-			classie.remove(document.getElementById("nav"),"smaller");
+		var distanceY = window.pageYOffset || document.documentElement.scrollTop;
+		if (distanceY > 300) {
+			classie.add(nav,"smaller");
+		} else {
+			if (classie.has(nav,"smaller")) {
+				classie.remove(nav,"smaller");
+			}
 		}
 	});
 
-	document.getElementById("nav").addEventListener("click", function(e) {
-		alert("debugz!");
+	document.getElementById("nav").addEventListener('click', function() {
+		if (classie.has(this,"smaller")) {
+			classie.remove(this,"smaller");
+		}
 	});
-}
-window.onload = init();
+};
