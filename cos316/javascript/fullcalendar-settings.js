a1d541ea4659c9e4eda69f7c6dc0d1c4a06ca751
Dan Leyzberg <dan.leyzberg@gmail.com>
2019-09-08 21:47:56 -0400

    316
    316

---
+++ cos316/javascript/fullcalendar-settings.js
@@ -0,0 +1,58 @@
+var beforeMouseoverCSS;
+$(document).ready(function() {
+  $('#calendar').fullCalendar({
+    googleCalendarApiKey: 'AIzaSyC1T8SMnMGA8ADvm397BX-kgj65OK1XQ3M',
+
+    eventSources: [{
+      googleCalendarId: '224q5o8nkur7aqmf7jv0b0tk1o@group.calendar.google.com',
+      color: '#DFDFDF',
+      textColor: 'black',
+      borderColor: 'white'
+    }, {
+      googleCalendarId: '9scv30k1kj4ji40cvp81mjrjj0@group.calendar.google.com',
+      color: '#C0C0C0',
+      textColor: 'black',
+      borderColor: 'white'
+    }],
+
+    eventMouseover: function(calEvent, jsEvent, view) {
+      beforeMouseoverCSS = $(this).css(["z-index","min-height","min-width","left","right","margin-right","opacity"]);
+      $(this).css({
+        "z-index": "100",
+        "min-height": "60px",
+        "min-width": "140px",
+        "left": "-10",
+        "right": "-10",
+        "margin-right": "0px",
+        "opacity": ".9"
+      });
+      return false;
+    },
+
+    eventMouseout: function(calEvent, jsEvent, view) {
+      $(this).css(beforeMouseoverCSS);
+      return false;
+    },
+
+    eventClick: function(calEvent, jsEvent, view) {
+      return false;
+    },
+
+    loading: function(bool) {
+      if (bool === false) {
+        $('#calendar #loading').toggle(false);
+      }
+    },
+
+    defaultView: 'agendaWeek',
+    minTime: "09:00",
+    maxTime: "23:00",
+    allDaySlot: false,
+    contentHeight: "auto",
+    slotDuration: "01:00:00",
+    /*slotEventOverlap: false*/
+    /*defaultDate: "2015-02-03",*/
+
+  });
+
+});
